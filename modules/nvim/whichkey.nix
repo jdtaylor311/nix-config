ba4f680b68bbfef0d7a32443456b6ecec692075f
jdtaylor311 <jdtaylor311@outlook.com>
2025-09-30 22:52:54 -0400

    refactor(nvim/which-key): migrate to new which-key spec using wk.add

--- modules/nvim/whichkey.nix
+++ modules/nvim/whichkey.nix
@@ -11,21 +11,20 @@
         layout = { align = 'left' },
       })
 
-      -- Telescope helpers (lazy load inside mappings)
-      local tb = function(fn)
+      local function tb(fn)
         return function()
           local ok, b = pcall(require, 'telescope.builtin')
           if ok and b[fn] then b[fn]() end
         end
       end
 
-      -- Toggle helpers
       local function toggle(option, on_msg, off_msg)
         return function()
           vim.opt_local[option] = not vim.opt_local[option]:get()
           vim.notify((vim.opt_local[option]:get() and on_msg) or off_msg, vim.log.levels.INFO)
         end
       end
+
       local diagnostics_virtual_text = true
       local function toggle_diagnostics()
         diagnostics_virtual_text = not diagnostics_virtual_text
@@ -33,7 +32,6 @@
         vim.notify('Diagnostics virtual_text: ' .. tostring(diagnostics_virtual_text), vim.log.levels.INFO)
       end
 
-      -- GitSigns helpers (only call if loaded)
       local function gs(fn)
         return function()
           local ok, g = pcall(require, 'gitsigns')
@@ -41,53 +39,53 @@
         end
       end
 
-      wk.register({
-        ["<leader>"] = {
-          f = {
-            name = "+file",
-            f = { tb('find_files'), 'Find Files' },
-            r = { tb('oldfiles'), 'Recent Files' },
-            g = { tb('live_grep'), 'Live Grep' },
-            b = { tb('buffers'), 'Buffers' },
-            h = { tb('help_tags'), 'Help' },
-            n = { function() vim.cmd.enew() end, 'New File' },
-            s = { function() vim.cmd.write() end, 'Save File' },
-            S = { function() vim.cmd('%write') end, 'Save All' },
-            q = { function() vim.cmd.quit() end, 'Quit' },
-            Q = { function() vim.cmd('qa') end, 'Quit All' },
-          },
-          g = {
-            name = "+git",
-            s = { gs('stage_hunk'), 'Stage Hunk' },
-            r = { gs('reset_hunk'), 'Reset Hunk' },
-            R = { gs('reset_buffer'), 'Reset Buffer' },
-            p = { gs('preview_hunk'), 'Preview Hunk' },
-            b = { gs('toggle_current_line_blame'), 'Toggle Blame' },
-            d = { gs('diffthis'), 'Diff This' },
-            D = { function() gs('diffthis')('~') end, 'Diff This (HEAD~)' },
-            n = { function() gs('nav_hunk')('next') end, 'Next Hunk' },
-            N = { function() gs('nav_hunk')('prev') end, 'Prev Hunk' },
-          },
-          l = { name = "+lsp" }, -- keys already mapped in lsp module
-          d = { name = "+debug" }, -- keys mapped in dap module
-          t = {
-            name = "+toggle",
-            r = { toggle('relativenumber', 'Relative number ON', 'Relative number OFF'), 'Relativenumber' },
-            n = { toggle('number', 'Number ON', 'Number OFF'), 'Line Numbers' },
-            w = { toggle('wrap', 'Wrap ON', 'Wrap OFF'), 'Wrap' },
-            s = { toggle('spell', 'Spell ON', 'Spell OFF'), 'Spell' },
-            c = { function()
-                    if vim.opt.colorcolumn:get()[1] then
-                      vim.opt.colorcolumn = {}
-                      vim.notify('ColorColumn OFF')
-                    else
-                      vim.opt.colorcolumn = { '80' }
-                      vim.notify('ColorColumn 80')
-                    end
-                  end, 'ColorColumn 80' },
-            d = { toggle_diagnostics, 'Diagnostics virtual text' },
-          },
-        },
+      -- New spec: use wk.add with explicit mappings and group declarations
+      wk.add({
+        -- Group declarations
+        { '<leader>f', group = '+file' },
+        { '<leader>g', group = '+git' },
+        { '<leader>l', group = '+lsp' },
+        { '<leader>d', group = '+debug' },
+        { '<leader>t', group = '+toggle' },
+
+        -- File group
+        { '<leader>ff', tb('find_files'), desc = 'Find Files' },
+        { '<leader>fr', tb('oldfiles'), desc = 'Recent Files' },
+        { '<leader>fg', tb('live_grep'), desc = 'Live Grep' },
+        { '<leader>fb', tb('buffers'), desc = 'Buffers' },
+        { '<leader>fh', tb('help_tags'), desc = 'Help' },
+        { '<leader>fn', function() vim.cmd.enew() end, desc = 'New File' },
+        { '<leader>fs', function() vim.cmd.write() end, desc = 'Save File' },
+        { '<leader>fS', function() vim.cmd('%write') end, desc = 'Save All' },
+        { '<leader>fq', function() vim.cmd.quit() end, desc = 'Quit' },
+        { '<leader>fQ', function() vim.cmd('qa') end, desc = 'Quit All' },
+
+        -- Git group
+        { '<leader>gs', gs('stage_hunk'), desc = 'Stage Hunk' },
+        { '<leader>gr', gs('reset_hunk'), desc = 'Reset Hunk' },
+        { '<leader>gR', gs('reset_buffer'), desc = 'Reset Buffer' },
+        { '<leader>gp', gs('preview_hunk'), desc = 'Preview Hunk' },
+        { '<leader>gb', gs('toggle_current_line_blame'), desc = 'Toggle Blame' },
+        { '<leader>gd', gs('diffthis'), desc = 'Diff This' },
+        { '<leader>gD', function() gs('diffthis')('~') end, desc = 'Diff This (HEAD~)' },
+        { '<leader>gn', function() gs('nav_hunk')('next') end, desc = 'Next Hunk' },
+        { '<leader>gN', function() gs('nav_hunk')('prev') end, desc = 'Prev Hunk' },
+
+        -- Toggle group
+        { '<leader>tr', toggle('relativenumber', 'Relative number ON', 'Relative number OFF'), desc = 'Relativenumber' },
+        { '<leader>tn', toggle('number', 'Number ON', 'Number OFF'), desc = 'Line Numbers' },
+        { '<leader>tw', toggle('wrap', 'Wrap ON', 'Wrap OFF'), desc = 'Wrap' },
+        { '<leader>ts', toggle('spell', 'Spell ON', 'Spell OFF'), desc = 'Spell' },
+        { '<leader>tc', function()
+            if vim.opt.colorcolumn:get()[1] then
+              vim.opt.colorcolumn = {}
+              vim.notify('ColorColumn OFF')
+            else
+              vim.opt.colorcolumn = { '80' }
+              vim.notify('ColorColumn 80')
+            end
+          end, desc = 'ColorColumn 80' },
+        { '<leader>td', toggle_diagnostics, desc = 'Diagnostics virtual text' },
       })
     '';
   };
